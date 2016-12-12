b74b567239637482c9d69183af93673c35174a9d
Pwootage <pwootage@gmail.com>
2016-12-11 20:42:55 -0700

    Models and such

--- src/main/scala/com/pwootage/metroidprime/randomizer/Prime1Item.scala
+++ src/main/scala/com/pwootage/metroidprime/randomizer/Prime1Item.scala
@@ -1,7 +1,13 @@
 package com.pwootage.metroidprime.randomizer
 
+import com.pwootage.metroidprime.templates.Vector3FScriptPropertyValue
+
 case class Prime1Item(name: String,
                       var count: Int,
                       item: Int,
                       capacity: Option[Int],
-                      amount: Option[Int])
+                      amount: Option[Int],
+                      model: Int,
+                      animSet: Int,
+                      animCharacter: Option[Int],
+                      rotation: Option[Vector3FScriptPropertyValue])
